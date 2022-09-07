d001302f144aff662f09107c0f57851576e15d43
uturkarslan <ugurturkarslan@gmail.com>
2022-09-07 22:39:11 +0200

    Get AKS creds

--- 03_install_azure_cli.sh
+++ 03_install_azure_cli.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
+# Get commandline arguments
+while (( "$#" )); do
+  case "$1" in
+    --resource-group)
+      $resourceGroupName="$2"
+      shift
+      ;;
+    --aks-name)
+      $aksName="$2"
+      shift
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
 # Update
 sudo apt-get update
 echo Y | sudo apt-get upgrade
 
 # Install
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+# Log in
+az login --use-device-code
+
+# Get AKS credentials
+az aks get-credentials \
+  --resource-group $resourceGroupName \
+  --name $aksName \
+  --overwrite-existing
